5f359cc2dad3381761a3bd82650cbcbe2b4e3912
itsiprikshit <prikshittekta@gmail.com>
2023-04-18 19:55:06 -0600

    country codes

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var dataObj = {
         snow: {},
         awnd: {}
     },
+    countries: [],
     landTopo: {},
     world: {}
 };
@@ -12,7 +13,7 @@ var dataObj = {
 const getVal = (feat, yeardata) => {
     var id = feat.id;
 
-    var country = COUNTRY_CODES.filter((c) => c.numeric == id);
+    var country = dataObj.countries.filter((c) => c.numeric == id);
 
     if (country.length) {
         country = country[0];
@@ -51,12 +52,19 @@ const updateGlobe = (datatype, selyear) => {
     var response = await fetch("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json");
     dataObj.landTopo = await response.json();
 
-    var responses = await Promise.all([fetch("./data/weather-yc-tavg.json"), fetch("./data/weather-yc-prcp.json"), fetch("./data/weather-yc-snow.json"), fetch("./data/weather-yc-wind.json")]);
+    var responses = await Promise.all([
+        fetch("./data/country-codes.json"),
+        fetch("./data/weather-yc-tavg.json"),
+        fetch("./data/weather-yc-prcp.json"),
+        fetch("./data/weather-yc-snow.json"),
+        fetch("./data/weather-yc-wind.json")
+    ]);
 
-    dataObj.weather.tavg = await responses[0].json();
-    dataObj.weather.prcp = await responses[1].json();
-    dataObj.weather.snow = await responses[2].json();
-    dataObj.weather.awnd = await responses[3].json();
+    dataObj.countries = (await responses[0].json()).countries;
+    dataObj.weather.tavg = await responses[1].json();
+    dataObj.weather.prcp = await responses[2].json();
+    dataObj.weather.snow = await responses[3].json();
+    dataObj.weather.awnd = await responses[4].json();
 
     var selectedYear = $("#curr-year").data("year");
     var selectedDataType = $("#data-type select").val();
